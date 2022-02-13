a610952d728cb9df9980a9f8841982bc34dfdce7
ZacharyGopinath <zachary.gopinath@ucc.on.ca>
2022-02-12 22:56:28 -0500

    Added box to keep track of score

--- rpsmain.js
+++ rpsmain.js
@@ -1,17 +1,20 @@
-document.getElementById("rockText").addEventListener("click", function win(){
+document.getElementById("rockText").addEventListener("click", function win() {
     document.getElementById("pickText").innerHTML = "Rock";
     document.getElementById("resultText").innerHTML = "Loss";
     document.getElementById("botPickText").innerHTML = "Paper";
+    document.getElementById('computerWins').textContent = parseInt(document.getElementById('computerWins').textContent) + 1;
 });
 
-document.getElementById("paperText").addEventListener("click", function win(){
+document.getElementById("paperText").addEventListener("click", function win() {
     document.getElementById("pickText").innerHTML = "Paper";
     document.getElementById("resultText").innerHTML = "Loss";
     document.getElementById("botPickText").innerHTML = "Scissors";
+    document.getElementById('computerWins').textContent = parseInt(document.getElementById('computerWins').textContent) + 1;
 });
 
-document.getElementById("scissorsText").addEventListener("click", function win(){
+document.getElementById("scissorsText").addEventListener("click", function win() {
     document.getElementById("pickText").innerHTML = "Scissors";
     document.getElementById("resultText").innerHTML = "Loss";
     document.getElementById("botPickText").innerHTML = "Rock";
+    document.getElementById('computerWins').textContent = parseInt(document.getElementById('computerWins').textContent) + 1;
 });
